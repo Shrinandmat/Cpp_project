65b2cbf80e4a25a0ca40c7e5d378627ac998e9f0
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-07-31 17:46:38 +0530

    Update Libary_management.cpp

--- Libary_management.cpp
+++ Libary_management.cpp
@@ -1,71 +1,73 @@
-#include<iostream>
-#include<string.h>
-#include<stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
-class Library{
-    public:
+class Library {
+public:
     int id;
     char name[100];
     char author[100];
     char student[100];
     int price;
     int pages;
-
 };
 
-int main(){
+int main() {
     Library lib[20];
-    int input=0;
-    int count=0;
+    int input = 0;
+    int count = 0;
 
-    while(input!=3){
-        cout<<"Enter 1 to input details like id, name, author, student, price, pages"<<endl;
-        cout<<"Enter 2 to display details"<<endl;
-        cout<<"Enter 3 to quit"<<endl;
-        cin>>input;
+    while (input != 3) {
+        cout << "Enter 1 to input details like id, name, author, student, price, pages" << endl;
+        cout << "Enter 2 to display details" << endl;
+        cout << "Enter 3 to exit" << endl;
+        cin >> input;
 
-        switch(input){
+        switch (input) {
             case 1:
-                start:
-                cout<<"Enter Book Id"<<endl;
-                cin>>lib[count].id;
-                cout<<"Enter Book Name"<<endl;
-                //cin>>lib[count].name;
-                cin.getline(lib[count].name,100,'$');
-                cout<<"Enter Book Author"<<endl;
-                //cin>>lib[count].author;
-                cin.getline(lib[count].author,100,'$');
-                cout<<"Enter Student Name"<<endl;
-                //cin>>lib[count].student;
-                cin.getline(lib[count].student,100,'$');
-                cout<<"Enter Book Pice"<<endl;
-                cin>>lib[count].price;
-                cout<<"Enter Book Pages"<<endl;
-                cin>>lib[count].pages;
-                count++;
+                if (count < 5) {
+                    cout << "Enter Book Id: ";
+                    cin >> lib[count].id;
+                    cin.ignore();  // To clear the newline character from the buffer
+                    cout << "Enter Book Name: ";
+                    cin.getline(lib[count].name, 100 ,'$');
+                    cout << "Enter Author Name: ";
+                    cin.getline(lib[count].author, 100 , '$');
+                    cout << "Enter Student Name: ";
+                    cin.getline(lib[count].student, 100 , '$');
+                    cout << "Enter Book Price: ";
+                    cin >> lib[count].price;
+                    cout << "Enter Book Pages: ";
+                    cin >> lib[count].pages;
+                    cin.ignore();  // Clear the newline character again
+                    count++;
+                } else {
+                    cout << "Library is full. Cannot add more books." << endl;
+                }
                 break;
+
             case 2:
-                for(int i=0;i<count;i++){
-                    cout<<"Book Id : "<<lib[i].id<<endl;
-                    cout<<"Book Name : "<<lib[i].name;
-                    cout<<"Book Author : "<<lib[i].author;
-                    cout<<"Book Student : "<<lib[i].student;
-                    cout<<"Book Price : "<<lib[i].price<<endl;
-                    cout<<"Book Pages : "<<lib[i].pages<<endl;
+                for (int i = 0; i < count; i++) {
+                    cout << "Book Id: " << lib[i].id << endl;
+                    cout << "Book Name: " << lib[i].name << endl;
+                    cout << "Book Author: " << lib[i].author << endl;
+                    cout << "Book Student: " << lib[i].student << endl;
+                    cout << "Book Price: " << lib[i].price << endl;
+                    cout << "Book Pages: " << lib[i].pages << endl;
+                    cout << "----------------------" << endl;
                 }
                 break;
 
             case 3:
-                exit(0);
+                cout << "Exiting..." << endl;
                 break;
 
             default:
-                cout<<"You have entered wrong value, please type again"<<endl;
-                goto start;
-
+                cout << "You have entered a wrong value, please try again." << endl;
         }
     }
 
+    return 0;
 }
